a1e202ebd1d066a397853c623dee24d9f1c0cbd1
Albert Hofmann <albhof@email.com>
2019-10-01 19:28:25 +0200

    initialize app

---
+++ src/components/wiki.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Wiki extends Component {
+    render () {
+        return(
+            <>
+
+            </>
+        );
+    }
+}
+
+export default Wiki;
